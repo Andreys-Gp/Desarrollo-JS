da3780ba973e20135f0e2233c2966e564c94935c
Brayan-Andreys <109758319+Andreys-Gp@users.noreply.github.com>
2024-02-15 23:28:26 -0500

    Update asincronia.js

--- profundidad/asincronia.js
+++ profundidad/asincronia.js
@@ -16,3 +16,86 @@ clearTimeout(temporizador);
 /* elimina */
 clearInterval(temporizador);
 
+/* callbacks */
+
+function cuadradoCallback(value, callback) {
+    let prueba = setTimeout(() => {
+        callback(value, value * value);
+
+    }, 0 || Math.random() * 1000);
+
+    clearTimeout(prueba);
+}
+cuadradoCallback(0, (value, result) => {
+    console.log("inicia callback");
+    console.log(`callback:${value},${result}`);
+    cuadradoCallback(1, (value, result) => {
+        console.log("inicia callback");
+        console.log(`callback:${value},${result}`);
+    })
+})
+
+
+/* promesas */
+
+function cuadradoPromise(value) {
+    if (typeof value !== "number") return Promise.reject(console.log(
+        `error, el valor ingresado "${value}" ingresado no es un numero `))
+
+    return new Promise((resolve, reject) => {
+        let pruebaPromise = setTimeout(() => {
+
+            resolve({
+                value: value,
+                result: value * value
+            });
+
+        }, 0 || Math.random() * 1000);
+
+        clearTimeout(pruebaPromise);
+
+    });
+}
+cuadradoPromise("")
+    .then((obj) => {
+        console.log("inicio promesa");
+        console.log(`promise:${obj.value},${obj.result}`);
+        return cuadradoPromise(1);
+    })
+    .then((obj) => {
+        console.log("inicio promesa");
+        console.log(`promise:${obj.value},${obj.result}`);
+        return cuadradoPromise(2);
+    })
+    .catch(err => console.log(err));
+
+
+
+/* ASYNC AWAIT */
+
+function cuadradoPromise(value) {
+    if (typeof value !== "number") return Promise.reject(console.log(
+        `error, el valor ingresado "${value}" ingresado no es un numero `))
+
+    return new Promise((resolve, reject) => {
+        let pruebaPromise = setTimeout(() => {
+
+            resolve({
+                value: value,
+                result: value * value
+            });
+
+        }, 0 || Math.random() * 1000);
+
+        clearTimeout(pruebaPromise);
+
+    });
+}
+
+async function funcionAsincronaDeclarada() {
+    try {
+        console.log("inicion de funcion Asyn");
+        let obj = cuadradoPromise(0);
+        console.log(`Asyn funtion:${obj.value},${obj.result}`)
+    } catch {}
+}
