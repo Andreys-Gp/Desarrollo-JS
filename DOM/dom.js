163fc8d3b30e16b12a004aa91c5087dbcb6c3c7c
Brayan-Andreys <109758319+Andreys-Gp@users.noreply.github.com>
2024-03-24 22:00:29 -0500

    reload

--- DOM/dom.js
+++ DOM/dom.js
@@ -2,3 +2,28 @@ const $linkDom =document.querySelector(".styles-ancla");
 
 console.log($linkDom.style)
 console.log($linkDom.getAttribute("style"))
+console.log($linkDom.style.backgroundColor)
+console.log(window.getComputedStyle($linkDom))
+console.log(getComputedStyle($linkDom).getPropertyValue("color"))
+
+$linkDom.style.setProperty("text-decoration","none")
+$linkDom.style.setProperty("background-color","white")
+
+
+/* variavles CSS */
+
+const $html=document.documentElement;
+const $body= document.body;
+
+ let varDarkColor=getComputedStyle($html).getPropertyValue("--dark-color");
+  let   varYellowkColor=getComputedStyle($html).getPropertyValue("--yellow-color");
+     
+     console.log(varDarkColor,varYellowkColor);
+    
+     $body.style.backgroundColor=varDarkColor;
+     $body.style.color=varYellowkColor;
+
+
+     $html.setProperty("--dark-color","pink");
+     varDarkColor=getComputedStyle($html).getPropertyValue("--dark-color");
+     $body.style.setProperty("backgroundcolor",varDarkColor);
